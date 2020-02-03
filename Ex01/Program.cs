8e8c6b9f18fec734a1529e0e6c12d61edd10a2b4
Staicu Adrian <adrianstacu888@gmail.com>
2020-02-04 00:06:39 +0200

    Add project files.

---
+++ Ex01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;  
+            int b = (a = 2) * a; // a devine 2 iar b devine 4
+            int c = b * (b = 5); // b devine 5
+            Console.WriteLine("a =" +a+ ", b =" +b+ ", c ="+c);
+
+            // afiseaza : a=2, b=5, c=20;
+        }
+    }
+}
---
+++ temaApp1/temaApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace temaApp1
+{
+    class Program
+    {
+        public class Asignare
+        {
+            public static void Main(string[] args)
+            {
+                int a = 3;
+                int b = (a = 2) * a;
+                int c = b * (b = 5);
+                Console.WriteLine(“a =” +a + “, b =” +b + “, c =” +c);
+                Console.Read();
+
+
+            }
+        }
+
+    }
+}
